3d61a141411521487c3d90d91edb77d0a54f0f03
hp <vishal.goswami@netzary.com>
2023-10-21 17:20:54 +0530

    changes

--- food_kitchen/settings.py
+++ food_kitchen/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "core",
+    'django.contrib.gis',
     "rest_framework",
     "corsheaders",
     "ajax_datatable",
